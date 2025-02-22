14db4155d1ee2c63c302c31f59669c86c345fd29
unknown <ibnufarkhan@student.telkomuniversity.ac.id>
2025-02-22 17:15:00 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/layout/Navbar';
+// import Footer from './components/layout/Footer';
+
+function App() {
+  return (
+    <Navbar />
+    // <Main />
+    // <Footer />
+  );
+}
+
+export default App;
